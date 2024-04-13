94b66acff0f9c6012e3286823bece57c3e899bde
gekkegozer2 <126954671+gekkegozer2@users.noreply.github.com>
2024-04-13 14:38:01 +0200

    Commit to the Bit
    Attempt at spawning system, failed

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -6,13 +6,13 @@ using UnityEngine.AI;
 public class EnemyFollow : MonoBehaviour
 {
     public NavMeshAgent enemy;
-    public Transform player;
+    private Transform player;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+      
     }
 
     // Update is called once per frame
