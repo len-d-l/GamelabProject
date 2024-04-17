aa4180eb2002e4d73713be02d66aadf61335418d
gekkegozer2 <126954671+gekkegozer2@users.noreply.github.com>
2024-04-17 20:54:53 +0200

    WAVES
    I AM ABOUT TO BUSTER

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -6,18 +6,18 @@ using UnityEngine.AI;
 public class EnemyFollow : MonoBehaviour
 {
     public NavMeshAgent enemy;
-    private Transform player;
+    private GameObject player;
 
 
     // Start is called before the first frame update
     void Start()
     {
-      
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.SetDestination(player.position);
+        enemy.SetDestination(player.transform.position);
     }
 }
