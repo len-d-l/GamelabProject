b091b870ab19cd36838b9819339d653a4a660219
LilSpectrum <demiri.lazic.len@gmail.com>
2024-06-06 12:52:21 +0200

    Started Multiplayer

---
+++ Assets/Scripts/Multiplayer/NetworkRunnerHandler.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+
+public class NetworkRunnerHandler : MonoBehaviour
+{
+    public NetworkRunner networkRunnerPrefab;
+
+    NetworkRunner networkRunner;
+
+    private void Start()
+    {
+        networkRunner = Instantiate(networkRunnerPrefab);
+        networkRunner.name = "Network Runner";
+
+        var clientTask = InitializeNetworkRunner(networkRunner, GameMode.AutoHostOrClient, NetAddress.Any(), SceneManager.GetActiveScene().buildIndex, null);
+    }
+
+    protected virtual Task InitializeNetworkRunner(NetworkRunner runner, GameMode gameMode, NetAddress address, SceneRef scene, Action<NetworkRunner> initialized)
+    {
+        var sceneManager = runner.GetComponents(typeof(MonoBehaviour)).OfType<INetworkSceneManager>().FirstOrDefault();
+
+        if (sceneManager == null )
+        {
+            // Handle networked objects that already exist
+            sceneManager = runner.gameObject.AddComponent<NetworkSceneManagerDefault>();
+        }
+
+        runner.ProvideInput = true;
+
+        return runner.StartGame(new StartGameArgs
+        {
+            GameMode = gameMode,
+            Address = address,
+            Scene = scene,
+            SessionName = "Level 1",
+            OnGameStarted = initialized,
+            SceneManager = sceneManager
+        });
+    }
+}
